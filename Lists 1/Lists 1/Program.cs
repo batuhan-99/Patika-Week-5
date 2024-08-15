eff1cc6906cc19aee90b509b95f09801f2fbc3ca
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-16 00:47:48 +0300

    Add files via upload

---
+++ Lists 1/Lists 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string>();
+            isimler.Add("Bülent Ersoy");
+            isimler.Add("Ajda Pekkan");
+            isimler.Add("Ebru Gündeş");
+            isimler.Add("Haadise");
+            isimler.Add("Hande yener");
+            isimler.Add("Tarkan");
+            isimler.Add("Funda Arar");
+            isimler.Add("Demet Akalin");
+            int sayi=1;
+            foreach(string isim in isimler)
+            {
+                Console.Write($"{sayi} - ");
+                Console.WriteLine($"{isim}");
+                sayi++;
+            }
+        }
+    }
+}
