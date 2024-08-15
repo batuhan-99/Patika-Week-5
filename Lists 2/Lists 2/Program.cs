f9f1dc776a56a91ff6c9da235e02bb895ad2224f
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-16 01:02:36 +0300

    Add files via upload

---
+++ Lists 2/Lists 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> kahve = new List<string>();
+            string isim;
+            int sayi = 1;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write($"Kahve {sayi} :");
+                isim= Console.ReadLine();
+                kahve.Add(isim);
+                sayi++;
+                
+            }
+            Console.WriteLine("Girilen Kahve İsimleri:");
+            foreach(string s in kahve)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
