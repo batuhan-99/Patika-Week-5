a9c1f82786aa156a12bc0cf8bdcb5ace0b5dc246
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-16 15:27:42 +0300

    Add files via upload

---
+++ IMDB List/IMDB List/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace IMDB_List
+{
+    public class Film
+    {
+        public string ismi { get; set; }
+        public double puan {  get; set; }
+
+        public Film(string Ismi, double Puan)
+        {
+            ismi = Ismi;
+            puan = Puan;
+        }
+
+    }
+}
---
+++ IMDB List/IMDB List/Program.cs
@@ -0,0 +1,50 @@
+﻿using IMDB_List;
+
+namespace IMDB_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Film> filmler = new List<Film>();
+            string devamcevabi;
+
+            while (true)
+            {
+                Console.Write("Film adı:");
+                string isim = Console.ReadLine();
+
+                Console.Write("IMDB puanı:");
+                double puan = Convert.ToDouble(Console.ReadLine());
+
+                Film yenifilm = new Film(isim, puan);
+                filmler.Add(yenifilm);
+                Console.Write("Yeni bir film eklemek istiyor musunuz? (Evet/Hayır): ");
+                devamcevabi = Console.ReadLine().ToLower();
+                if (devamcevabi == "hayır")
+                    break;
+
+            }
+            Console.WriteLine("Filmler:");
+            foreach (Film filmadi in filmler)
+            {
+                Console.WriteLine(filmadi.ismi);
+            }
+            Console.WriteLine("Puanı 4 ile 9 arasında olanlar");
+            foreach (Film filmadi in filmler)
+            {
+                if (filmadi.puan >= 4.0 && filmadi.puan <= 9.0)
+                    Console.WriteLine(filmadi.ismi);
+            }
+            Console.WriteLine("İsmi A harfi ile başlayan filmler:");
+            foreach (Film filmadi in filmler)
+            {
+                if (filmadi.ismi.StartsWith("A", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"İsim: {filmadi.ismi}, IMDB Puanı: {filmadi.puan}");
+                }
+            }
+
+        }
+    }
+}
