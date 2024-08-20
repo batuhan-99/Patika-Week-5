e2be0338242655ab33e461d54f7396711094b43d
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-20 14:11:48 +0300

    Add files via upload

---
+++ Pratik-TryCatch/Pratik-TryCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool devam = true;
+            Console.WriteLine("Lütfen bir sayi giriniz:");
+            while (devam)
+            {      
+                try
+                {        
+                double sayi = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(Math.Pow(sayi, 2));
+                devam = false;
+                }            
+                catch ( FormatException ex)
+                {
+                    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            
+                }
+            }
+        }
+    }
+}
