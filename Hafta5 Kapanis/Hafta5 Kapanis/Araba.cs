0f90aca353abd9a6687ab35a6979cd9a745c6830
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-21 01:13:24 +0300

    Add files via upload

---
+++ Hafta5 Kapanis/Hafta5 Kapanis/Araba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta5_Kapanis
+{
+    public class Araba
+    {
+        public DateTime UretimTarihi { get;  }
+        public string serinumarasi {  get; set; }
+        public string marka { get; set; }
+        public string model { get; set; }
+        public string renk { get; set; }
+        public int kapisayisi { get; set; }
+
+        public Araba()
+        {
+            UretimTarihi = DateTime.Now;
+        }
+
+    }
+}
---
+++ Hafta5 Kapanis/Hafta5 Kapanis/Program.cs
@@ -0,0 +1,75 @@
+﻿using Hafta5_Kapanis;
+
+namespace Hafta5_Kapanis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cevap;
+            List<Araba> arabalar = new List<Araba>();
+
+            while (true)
+            {         
+                Console.WriteLine("Araba üretmek istiyor musunuz? (E/H)");
+                cevap = Console.ReadLine().ToLower();
+                
+
+                if (cevap == "h")
+                    break;
+                else if(cevap != "e")
+                {
+                    Console.WriteLine("Geçersiz cevap. ");
+                    continue;
+                }
+                yeniden:
+                Araba araba = new Araba();
+                Console.Write("Seri Numarası: ");
+                araba.serinumarasi = Console.ReadLine();
+
+                Console.Write("Marka: ");
+                araba.marka = Console.ReadLine();
+
+                Console.Write("Model: ");
+                araba.model = Console.ReadLine();
+
+                Console.Write("Renk: ");
+                araba.renk = Console.ReadLine();
+
+                kapisayisiGir:
+                Console.Write("Kapı Sayısı: ");
+                try
+                {
+                    araba.kapisayisi = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Geçersiz bir değer girdiniz, lütfen sayısal bir değer giriniz.");
+                    goto kapisayisiGir;
+                }
+                arabalar.Add(araba);
+                Console.WriteLine("Araba başarıyla üretildi!");
+                Console.Write("Başka bir araba üretmek istiyor musunuz? (E/H): ");
+
+                cevap = Console.ReadLine().ToLower();
+                if (cevap == "h")
+                    break;
+                else if (cevap != "e")
+                {
+                    Console.WriteLine("Geçersiz cevap. ");
+                    continue;
+                }
+                else if(cevap == "e")
+                    goto yeniden;
+                
+            }
+            Console.WriteLine("\nÜretilen Arabalar:");
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine($"Seri Numarası: {araba.serinumarasi}, Marka: {araba.marka}");
+            }
+
+            Console.WriteLine("Program sona erdi. İyi günler!");
+        }
+    }
+}
